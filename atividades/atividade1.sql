a0ca3f9c99ca4d97a7391ff3fdbffab04c8acb84
icaro gavazza <icarodgl@gmail.com>
2017-09-06 14:34:21 -0300

    Update atividade1.sql

--- atividades/atividade1.sql
+++ atividades/atividade1.sql
@@ -39,6 +39,41 @@ CREATE VIEW top_5_cidades_caras AS
  GROUP BY (cidade.nome, estado.nome, pais.nome)
  ORDER BY preco desc LIMIT 5;
  
+ /* 
+ * 10 primeiras casas no ES  à venda por ordem dos mais caros e que tenham preço e area preenchidos
+ */
+CREATE VIEW casa_valorizadas_ES AS SELECT imovel.id, imovel.preco, imovel.area, imovel.data, cidade.nome as cidade, estado.nome as estado, categoria.nome as categoria , categoria.tipo as tipo
+	FROM imovel
+		INNER JOIN cidade ON (imovel.fk_cidade_id = cidade.id)
+		INNER JOIN estado ON (cidade.fk_estado_id = estado.id)
+		INNER JOIN cat_imo ON (imovel.id = cat_imo.fk_imovel_id)
+		INNER JOIN categoria ON (cat_imo.fk_categoria_id = categoria.id)
+		where 
+			categoria.nome = 'casas' and
+			categoria.tipo = 'à venda' and
+			estado.nome = 'ES'and
+			imovel.preco > 1 and
+			imovel.area > 1
+		order by (imovel.preco) desc
+		limit 10;
+ 
+ /* 
+ *in 
+ */
+SELECT imovel.id, imovel.preco, imovel.area, imovel.data, cidade.nome as cidade, estado.nome as estado, categoria.nome as categoria , categoria.tipo as tipo
+	FROM imovel
+		INNER JOIN cidade ON (imovel.fk_cidade_id = cidade.id)
+		INNER JOIN estado ON (cidade.fk_estado_id = estado.id)
+		INNER JOIN cat_imo ON (imovel.id = cat_imo.fk_imovel_id)
+		INNER JOIN categoria ON (cat_imo.fk_categoria_id = categoria.id)
+		where 
+			categoria.nome = 'casas' and
+			categoria.tipo in ('à venda', 'para alugar') and
+			estado.nome in ('ES', 'RJ') and
+			imovel.preco > 1 and
+			imovel.area > 1
+		limit 100;
+    
  /*
  * Média de preços por categoria
  */
